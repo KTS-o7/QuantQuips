7fed5b092779487a86e82164bc4202af8b8c9dc3
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-02-04 00:37:49 +0530

    Add Backtesting

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from langchain.llms import CTransformers
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
+import subprocess
 
 # Function to load documents 
 def load_documents():
@@ -262,35 +263,10 @@ print(max_tuple2[0],max_tuple2[1],max_tuple2[2])
 
         # Display output
         outputList = output.split()
-        shortPrd = int(outputList[0])
-        longPrd = int(outputList[1])    
-        portVal = round(float(outputList[2])/10, 2)
-        grossProfit = round(portVal - cash, 2)
-
-        st.markdown(f"""<div style="color: white; font-size: 24px; font-weight: bold;">
-<br>Recommended Values:<br>
-- Short period value is: {shortPrd}<br>
-- Long period value is: {longPrd}<br>
-- Portfolio value is: {portVal}<br>
-- Gross profit is   : {grossProfit}</div>
-""",unsafe_allow_html=True)
-
-        shortPrd = int(outputList[0])
-longPrd = int(outputList[1])
-portVal = round(float(outputList[2])/10, 2)
-grossProfit = round(portVal - cash, 2)
-
-st.markdown(f"""<div style="color: white; font-size: 24px; font-weight: bold;">
-<br>## Recommended Values:<br>
-- Short period value is: {shortPrd}<br>
-- Long period value is: {longPrd}<br>
-- Portfolio value is: {portVal}<br>
-- Gross profit is   : {grossProfit}</div>
-""",unsafe_allow_html=True)
-
-
-
-
+        st.text(outputList)
+       #st.text(output)
+        
+## LLM PART
 elif page_selection == "LLM":
     st.title("QuantQuip-ChatBot")
    
@@ -352,32 +328,7 @@ elif page_selection == "LLM":
             for i in range(len(st.session_state['generated'])):
                 message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
                 message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+## ABOUT US
 
 elif page_selection == "About Us":
     st.title("About Us")
@@ -423,6 +374,35 @@ elif page_selection == "About Us":
         Python Developer
         """)
 
+elif page_selection == "Backtesting":
+    def save_strategy(strategy_code):
+        with open('strategies.py', 'w') as file:
+                file.write(strategy_code)
 
+    def run_trader(principal_amount):
+        try:
+            result = subprocess.run(['python', 'trader.py', str(principal_amount)], capture_output=True, text=True, check=True)
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            return e.stderr
+    st.title("Automated Back Testing")
+
+    # Text area for user input (strategy code)
+    strategy_code = st.text_area("Enter your strategy code here:")
+
+    # Input for initial principal amount
+    principal_amount = st.number_input("Enter initial principal amount:", value=1000000000.0)
+
+    # Button to save the strategy and run the trader
+    if st.button("Save and Run Trader"):
+        save_strategy(strategy_code)
+        st.write("Strategy saved successfully!")
+        
+        # Run trader.py with initial principal amount as argument
+        output = run_trader(principal_amount)
+        
+        # Display the output
+        st.subheader("Trader Output:")
+        st.text(output)
 
 
---
+++ strategies.py
@@ -0,0 +1,27 @@
+# Create a Stratey
+import backtrader as bt
+
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.dataclose[0] < self.dataclose[-1]:
+            # current close less than previous close
+
+            if self.dataclose[-1] < self.dataclose[-2]:
+                # previous close less than the previous close
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                self.buy()
